41fdae1ae4052098ad378bdea6a0da1440bfedd8
quannytsunami <nguy1491@gmail.com>
2020-09-28 16:02:40 -0500

    Restart LinkedIn Learning course at JUnit basics. 1st failure.

--- ISBNValidator/src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
+++ ISBNValidator/src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
@@ -2,11 +2,5 @@ package com.virtualpairprogrammers.isbntools;
 
 public class ValidateISBN {
 
-	public boolean checkISBN(int isbn) {
-		if (isbn == 140449116) {
-			return true;
-		}
-		return false;
-	}
 
 }
